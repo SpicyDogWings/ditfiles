d540d963940a2eea2535253270e8efd7be096f52
spaceunandev <jocsan.fonseca@di.unanleon.edu.ni>
2025-06-09 15:52:31 -0600

    Mon Jun  9 03:52:31 PM CST 2025

---
+++ dot_config/nvim/lua/config/options.lua
@@ -0,0 +1,13 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+if vim.g.neovide then
+  vim.o.guifont = "0xProto Nerd Font:h14"
+  vim.g.neovide_title_background_color =
+    string.format("%x", vim.api.nvim_get_hl(0, { id = vim.api.nvim_get_hl_id_by_name("Normal") }).bg)
+  vim.g.neovide_title_text_color = "pink"
+  vim.g.neovide_cursor_animate_in_insert_mode = true
+  vim.g.neovide_cursor_animate_command_line = true
+  vim.g.neovide_cursor_vfx_mode = "sonicboom"
+end
